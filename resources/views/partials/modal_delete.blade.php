51af77406e8ebc9ea3ec9fe47ee6a2546d640a5c
Hycarus <mirkomarasco08@gmail.com>
2024-01-10 15:07:48 +0100

    changed modal

--- resources/views/partials/modal_delete.blade.php
+++ resources/views/partials/modal_delete.blade.php
@@ -1,12 +1,10 @@
 <!-- Modal -->
-<div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="deleteModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
+<div class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="deleteModalLabel">Attenzione</h5>
-                <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
+                <h1 class="modal-title fs-5" id="deleteModalLabel">Attenzione</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
                 <p>Sei sicuro di voler cancellare
